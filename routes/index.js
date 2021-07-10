1ea2db9a7adf25de02823e19000177376d4a698f
patexo <patexowin@hotmail.es>
2021-07-10 17:42:36 +0200

    Step 1.2: Application layout

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+// Contacto page.
+router.get('/contacto', (req, res, next) => {
+  res.render('contacto');
 });
 
 module.exports = router;
